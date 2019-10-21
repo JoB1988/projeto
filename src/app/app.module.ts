0a723bb97653b86e3ad3e449116f35f475094721
xb216201 <p000jonathan.fernandes.benevides@prservicos.com.br>
2019-10-21 16:54:43 -0300

    criação dos componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { PokemonComponent } from './pokemons/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
