e77d33a103e981311cc36f69862137d420cb1c89
JoB1988 <jonathan_benevides@hotmail.com>
2020-01-25 20:56:59 -0300

    forms validados

--- src/app/cep/cep.component.ts
+++ src/app/cep/cep.component.ts
@@ -10,42 +10,58 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class CepComponent implements OnInit {
 
-  public addressMask = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+  public alphanumericMask = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+  public lettersMask = 'LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL';
+  public lettersPattern = { 'L': { pattern: new RegExp(/^[a-zA-ZãõñáéíóúÁÉÍÓÚçÇ ]*$/), symbol: 'L' } };
   public alphanumericPatterns = { 'X': { pattern: new RegExp(/^[a-zA-Z0-9ãõñáéíóúÁÉÍÓÚçÇ ]*$/), symbol: 'X' } };
   states = [
-    { state: 'AC', name: 'Acre', capital: 'Rio Branco' },
-    { state: 'AL', name: 'Alagoas', capital: 'Maceió' },
-    { state: 'AM', name: 'Amazonas', capital: 'Manaus' },
-    { state: 'AP', name: 'Amapá', capital: 'Macapá' },
-    { state: 'BA', name: 'Bahia', capital: 'Salvador' },
-    { state: 'CE', name: 'Ceará', capital: 'Fortaleza' },
-    { state: 'DF', name: 'Distrito Federal', capital: 'Brasília' },
-    { state: 'ES', name: 'Espírito Santo', capital: 'Vitória' },
-    { state: 'GO', name: 'Goiás', capital: 'Goiânia' },
-    { state: 'MA', name: 'Maranhão', capital: 'São Luis' },
-    { state: 'MG', name: 'Minas Gerais', capital: 'Belo Horizonte' },
-    { state: 'MS', name: 'Mato Grosso do Sul', capital: 'Campo Grande' },
-    { state: 'MT', name: 'Mato Grosso', capital: 'Cuiabá' },
-    { state: 'PA', name: 'Paraíba', capital: 'João Pessoa' },
-    { state: 'PE', name: 'Pernambuco', capital: 'Recife' },
-    { state: 'PI', name: 'Piauí', capital: 'Teresina' },
-    { state: 'PR', name: 'Paraná', capital: 'Curitiba' },
-    { state: 'RJ', name: 'Rio de Janeiro', capital: 'Rio de Janeiro' },
-    { state: 'RN', name: 'Rio Grande do Norte', capital: 'Natal' },
-    { state: 'RO', name: 'Rondônia', capital: 'Porto Velho' },
-    { state: 'RR', name: 'Roraima', capital: 'Boa Vista' },
-    { state: 'RS', name: 'Rio Grande do Sul', capital: 'Porto Alegre' },
-    { state: 'SC', name: 'Santa Catarina', capital: 'Florianópolis' },
-    { state: 'SE', name: 'Sergipe', capital: 'Aracajú' },
-    { state: 'SP', name: 'São Paulo', capital: 'São Paulo' },
-    { state: 'TO', name: 'Tocantins', capital: 'Palmas' }
+    'AC',
+    'AL',
+    'AM',
+    'AP',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MG',
+    'MS',
+    'MT',
+    'PA',
+    'PE',
+    'PI',
+    'PR',
+    'RJ',
+    'RN',
+    'RO',
+    'RR',
+    'RS',
+    'SC',
+    'SE',
+    'SP',
+    'TO',
   ];
 
-  public cepForm: BehaviorSubject<FormGroup> = new BehaviorSubject(this.formBuilder.group({
+  public cepForm$: BehaviorSubject<FormGroup> = new BehaviorSubject(this.formBuilder.group({
     // tslint:disable-next-line: max-line-length
-    cep: ['', Validators.required],
-    address: ['', Validators.compose([Validators.required])],
-    state: ['', Validators.required],
+    pessoa: this.formBuilder.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      nasc: ['', Validators.required],
+      cpf: ['', Validators.required],
+      rg: ['', Validators.required],
+      profissao: ['', Validators.required],
+    }),
+    direcao: this.formBuilder.group({
+      cep: ['', Validators.required],
+      logradouro: ['', Validators.required],
+      cidade: ['', Validators.required],
+      bairro: ['', Validators.required],
+      complemento: [''],
+      uf: ['', Validators.required],
+      referencia: ['']
+    }),
   }));
 
   constructor(public readonly formBuilder: FormBuilder, private cepService: CepService) { }
@@ -53,17 +69,17 @@ export class CepComponent implements OnInit {
   ngOnInit() { }
 
   public searchDirectionByCep() {
-    if (this.cepForm.value.controls.cep.invalid) {
+    if (this.cepForm$.value.controls.cep.invalid) {
       return;
     }
-    this.cepService.getAddress(this.cepForm.value.controls.cep.value).subscribe(response => {
-      this.cepForm.value.controls.address.setValue(response.logradouro);
-      this.cepForm.value.controls.state.setValue(response.uf);
+    this.cepService.getAddress(this.cepForm$.value.controls.cep.value).subscribe(response => {
+      this.cepForm$.value.controls.logradouro.setValue(response.logradouro);
+      this.cepForm$.value.controls.uf.setValue(response.uf);
     });
   }
 
   public saveForm() {
-    this.cepService.saveForm(this.cepForm.value.value).subscribe(response => {
+    this.cepService.saveForm(this.cepForm$.value.value).subscribe(response => {
 
     });
   }
