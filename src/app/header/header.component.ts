199290454012cc7d369edf2fff2aff08515e85eb
Jonathan Fernandes Benevides <jfernabe@everis.com>
2020-02-19 16:01:31 -0300

    ajuste do header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,36 +1,15 @@
-import { Component, OnInit, HostListener, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  private div = '<ul class="navigation-list!"' +
-    '<li><a [routerLink]="["/home"]" routerLinkActive="active">Home</a></li>' +
-    '<li><a [routerLink]="["/ceps"]" routerLinkActive="active">Ceps</a></li>' +
-    '<li><a [routerLink]="["/pokemons"]" routerLinkActive="active">Pokemons</a></li>' +
-    '</ul>';
-
-  @ViewChild('navigation', { static: true }) navigation: ElementRef;
+export class HeaderComponent {
 
   constructor() { }
 
-  ngOnInit() { }
-
-  @HostListener('window:resize', ['$event']) onResize(): string {
-    // if (window.innerWidth < 500) {
-    //   this.navigation.appendChild(this.divs[0]);
-    //   this.navigation.appendChild(this.divs[1]);
-    // } else {
-    //   this.navigation.appendChild(this.divs[1]);
-    //   this.navigation.appendChild(this.divs[0]);
-    // }
-    // console.log(this.navigation);
-    // console.log(this.navigation.nativeElement);
-    // this.navigation.nativeElement.insertAdjacentHTML('beforeend', this.div);
-    return '';
-  }
-
+  // @HostListener('window:resize', ['$event']) public deviceClass(): string {
+  //   return '';
+  // }
 }
